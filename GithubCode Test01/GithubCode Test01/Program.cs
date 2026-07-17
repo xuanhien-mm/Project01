ddfa0bcc04579974911a580b7c32e59dcb42deed
NguyenXuanHien <xuanhien@incomda.com>
2026-07-17 14:20:10 +0700

    cập nhật dòng 2

---
+++ GithubCode Test01/GithubCode Test01/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GithubCode_Test01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, my name is Hien, this is the first project in GITHUB");
+            Console.WriteLine("Xin chào đây là dòng lệnh số 2");
+        }
+    }
+}
